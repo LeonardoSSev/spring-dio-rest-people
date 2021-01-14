eb56f699a5e63e75cc23228fd9979aa85fde6432
LeonardoSSev <leonardossev@gmail.com>
2021-01-14 09:41:45 -0300

    feat: Starts Spring project

---
+++ src/test/java/com/leonardossev/dio/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardossev.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
